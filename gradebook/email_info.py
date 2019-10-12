54cd3265c4ad49c72bbb0ee0f667c01664fb17fa
Charlotte <larryandshellie@yahoo.com>
2019-10-11 20:35:01 -0500

    email

--- gradebook/email_info.py
+++ gradebook/email_info.py
@@ -2,5 +2,5 @@
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'tynercreeksoftware@gmail.com'
-EMAIL_HOST_PASSWORD = 'Principal1'
+EMAIL_HOST_PASSWORD = 'Principal1!'
 EMAIL_PORT = 587
