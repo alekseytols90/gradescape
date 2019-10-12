a8b1626ad1ab5147859b77784ce04f717133777e
Charlotte <larryandshellie@yahoo.com>
2019-10-12 17:31:21 -0500

    gradebook

--- mygrades/views.py
+++ mygrades/views.py
@@ -1,4 +1,4 @@
-from admin.placeholderadmin import FrontendEditableAdminMixin
+from gradebook.admin.placeholderadmin import FrontendEditableAdminMixin
 from django.shortcuts import render, redirect, render_to_response
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
