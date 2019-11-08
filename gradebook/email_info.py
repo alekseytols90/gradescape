7c89536b1f2490d41e9c482fe80046665aa77e47
Charlotte <larryandshellie@yahoo.com>
2019-11-08 09:59:00 -0600

    changed email account information for the notifications

--- gradebook/email_info.py
+++ gradebook/email_info.py
@@ -1,6 +1,6 @@
 #for gmail
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'tynercreeksoftware@gmail.com'
-EMAIL_HOST_PASSWORD = 'Principal1'
+EMAIL_HOST_USER = 'yourepiconline@gmail.com'
+EMAIL_HOST_PASSWORD = 'NSURocks!'
 EMAIL_PORT = 587
