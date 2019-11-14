b5cf090c07ac2358de8b17bcd2c81bc26ad95027
butsoleg <olegbuts09@gmail.com>
2019-11-14 21:47:28 +0300

    email button for late and weekly assignment

--- gradebook/email_info.py
+++ gradebook/email_info.py
@@ -1,4 +1,6 @@
 #for gmail
+import os
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'yourepiconline@gmail.com'
