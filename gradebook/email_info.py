5187e4911bfca6d79b86a4549f75d3634689e405
Charlotte <larryandshellie@yahoo.com>
2019-11-26 07:00:02 -0600

    email settings

--- gradebook/email_info.py
+++ gradebook/email_info.py
@@ -3,6 +3,6 @@ import os
 EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.smtp.EmailBackend")
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'yourepiconline@gmail.com'
-EMAIL_HOST_PASSWORD = 'NSURocks!'
+EMAIL_HOST_USER = 'yourschoolwebsitedonotreply@gmail.com'
+EMAIL_HOST_PASSWORD = 'Principal1234!'
 EMAIL_PORT = 587
