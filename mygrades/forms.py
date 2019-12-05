73450252d5ffa780239c36a81ed6ac39ac20b6c0
butsoleg <olegbuts09@gmail.com>
2019-12-05 17:34:01 +0300

    recover changes for year options, reporting calc, std form

--- mygrades/forms.py
+++ mygrades/forms.py
@@ -97,24 +97,21 @@ TRACKING = [
 
 ]
 
+
 def generate_semester_choices(for_choice_field=False):
-    """ Generate options for per semester for the current academic year and the next
+    """ Generate options for the current academic year and the next
 
         1- get current year
         2- if I'm before Jul 30 of c_year, base:  (c_year-1)-c_year    -> start by c_year-1
            if I'm after  Jul 30 of c_year, base: c_year-(c_year+1)    -> start by c_year
 
         Examples:
-        19-20 A
-        19-20 B
-        20-21 A
-        20-21 B
+        19-20
+        20-21
 
         Formulation:
-        start-(start+1)-A
-        start-(start+1)-B
-        (start+1)-(start+2)-A
-        (start+1)-(start+2)-B
+        start-(start+1)
+        (start+1)-(start+2)
     """
     now = timezone.now()
     current_year = now.year
@@ -126,10 +123,8 @@ def generate_semester_choices(for_choice_field=False):
         start = current_year - 1
 
     options = []
-    options.append("%s-%s-%s" % (str(start)[2:],str(start+1)[2:], "A"))
-    options.append("%s-%s-%s" % (str(start)[2:],str(start+1)[2:], "B"))
-    options.append("%s-%s-%s" % (str(start+1)[2:],str(start+2)[2:], "A"))
-    options.append("%s-%s-%s" % (str(start+1)[2:],str(start+2)[2:], "B"))
+    options.append("%s-%s" % (str(start)[2:],str(start+1)[2:]))
+    options.append("%s-%s" % (str(start+1)[2:],str(start+2)[2:]))
 
     if for_choice_field:
         choices = []
@@ -463,7 +458,6 @@ class StandardSetupForm(forms.ModelForm):
         }
 
     def __init__(self, *args, **kwargs):
-        instance = kwargs.pop("instance", None)
         request = kwargs.pop("request", None)
         super(StandardSetupForm, self).__init__(*args, **kwargs)
 
@@ -1150,7 +1144,6 @@ def gen_overall_data_progress_weekly(academic_semester, student, semester, quart
         overall.update({subject:[]})
 
         for week in range(start_week,start_week+9): #for each week e.g (10-18)
-            total = 0
             gbs = gradebooks.filter(curriculum=item.curriculum, week=week)
 
             if gbs.count() == 0:
@@ -1160,7 +1153,7 @@ def gen_overall_data_progress_weekly(academic_semester, student, semester, quart
             total = 0
             if gbs.count() == 1:
                 gb = gbs[0]
-                total += round(gb.grade * (enr.weight/100))
+                total += gb.grade  # no need to use weight since single course
             overall[subject].append(total)
 
 
